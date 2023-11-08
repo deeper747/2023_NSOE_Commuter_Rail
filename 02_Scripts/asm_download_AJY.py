a91087b699926cf229802193ac22d656b7d0290b
deeper747 <pisces747@gmail.com>
2023-11-08 16:00:51 -0500

    run one day

--- 02_Scripts/asm_download_AJY.py
+++ 02_Scripts/asm_download_AJY.py
@@ -12,8 +12,6 @@ in the US from https://asm.transitdocs.com/ at a specified interval.
 import requests, json, time, os
 from datetime import datetime
 
-#os.chdir('../01_Data/01_Source') #don't know why I couldn't change the directory. Might because I use google drvie syncing, but VScode don't have the access to the folder
-
 def download_json_data(url):
     try:
         response = requests.get(url)
@@ -31,9 +29,10 @@ def save_to_json(data, filename):
         json.dump(data, outfile, indent=2)
         print(f"Data saved to {filename}")
 
+## run for a day
 def main():
     backend_url = "https://asm-backend.transitdocs.com/map"  # the backend URL
-    total_loops = 7 * 24 * 60 * 3  # 7 days * 24 hours * 60 minutes/hour * 3 (for 20 seconds intervals)
+    total_loops = 24 * 60 * 3  # 24 hours * 60 minutes/hour * 3 (for 20 seconds intervals)
     current_loop = 0
     data_list = []
 
@@ -60,6 +59,37 @@ def main():
         # Wait for 20 seconds before making the next request
         time.sleep(20)
         current_loop += 1
+
+## run for a week
+# def main():
+#     backend_url = "https://asm-backend.transitdocs.com/map"  # the backend URL
+#     total_loops = 7 * 24 * 60 * 3  # 7 days * 24 hours * 60 minutes/hour * 3 (for 20 seconds intervals)
+#     current_loop = 0
+#     data_list = []
+
+#     while current_loop < total_loops:
+#         json_data = download_json_data(backend_url)
+#         if json_data:
+#             # Process the downloaded JSON data here
+#             print("Downloaded JSON data:", json.dumps(json_data, indent=2))
+#             data_list.append(json_data)
+
+#         # Retrieve current date and time. 
+#         date_time = datetime.now()
+
+#         # Save outfile name.
+#         date_time_clean = str(date_time).replace('-','_',)
+#         date_time_clean = date_time_clean.replace(' ','_',)
+#         date_time_clean = date_time_clean.replace(':','_',)
+#         date_time_clean = date_time_clean.replace('.','_',)
+#         outfile = '../01_Data/01_Source/' + date_time_clean + '.json'
+
+#         # Save the data to a JSON file
+#         save_to_json(data_list, outfile)
+        
+#         # Wait for 20 seconds before making the next request
+#         time.sleep(20)
+#         current_loop += 1
 
 if __name__ == "__main__":
     main()
