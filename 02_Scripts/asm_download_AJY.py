c4d71811390fab818a14b1f4238ac161dbb87bf6
deeper747 <pisces747@gmail.com>
2023-12-26 20:03:46 -0500

    NEW SCRAPING ENDED

--- 02_Scripts/asm_download_AJY.py
+++ 02_Scripts/asm_download_AJY.py
@@ -1,7 +1,7 @@
 '''
-Contributors: Jia-Shen Tsai
+Contributors: Jia-Shen Tsai,  Wendy Wen, Zhengqi Jiao, Miaojun Pang, Alexander Yoshizumi
 
-Last Updated: 2023-11-12
+Last Updated: 2023-12-25
 
 Description: When run, script pulls time-location data for passenger trains
 in the US from https://asm.transitdocs.com/ at a specified interval.
@@ -10,8 +10,6 @@ in the US from https://asm.transitdocs.com/ at a specified interval.
 
 import requests, json, time, os
 from datetime import datetime
-
-os.makedirs("../Data/ASM")
 
 def download_json_data(url):
     try:
@@ -30,16 +28,16 @@ def save_to_json(data, filename):
         json.dump(data, outfile, indent=2)
         print(f"Data saved to {filename}")
 
-## run for a day
+
+## run for two weeks
 def main():
     backend_url = "https://asm-backend.transitdocs.com/map"  # the backend URL
-    total_loops = 24 * 60 * 3  # 24 hours * 60 minutes/hour * 3 (for 20 seconds intervals)
+    total_loops = 14 * 24 * 60 * 3  # 14 days * 24 hours/day * 60 minutes/hour * 3 (for 20 seconds intervals)
     current_loop = 0
 
     while current_loop < total_loops:
         json_data = download_json_data(backend_url)
         if json_data:
-            # Process the downloaded JSON data here
             print("Downloaded JSON data:", json.dumps(json_data, indent=2))
 
         # Retrieve current date and time. 
@@ -50,7 +48,7 @@ def main():
         date_time_clean = date_time_clean.replace(' ','_',)
         date_time_clean = date_time_clean.replace(':','_',)
         date_time_clean = date_time_clean.replace('.','_',)
-        outfile = '..\\01_Data\\01_Source\\ASM\\' + date_time_clean + '.json'
+        outfile = './01_Data/01_Source/ASM/' + date_time_clean + '.json'
 
         # Save the data to a JSON file
         save_to_json(json_data, outfile)
@@ -58,37 +56,6 @@ def main():
         # Wait for 20 seconds before making the next request
         time.sleep(20)
         current_loop += 1
-
-## run for a week
-# def main():
-#     backend_url = "https://asm-backend.transitdocs.com/map"  # the backend URL
-#     total_loops = 7 * 24 * 60 * 3  # 7 days * 24 hours * 60 minutes/hour * 3 (for 20 seconds intervals)
-#     current_loop = 0
-#     data_list = []
-
-#     while current_loop < total_loops:
-#         json_data = download_json_data(backend_url)
-#         if json_data:
-#             # Process the downloaded JSON data here
-#             print("Downloaded JSON data:", json.dumps(json_data, indent=2))
-#             data_list.append(json_data)
-
-#         # Retrieve current date and time. 
-#         date_time = datetime.now()
-
-#         # Save outfile name.
-#         date_time_clean = str(date_time).replace('-','_',)
-#         date_time_clean = date_time_clean.replace(' ','_',)
-#         date_time_clean = date_time_clean.replace(':','_',)
-#         date_time_clean = date_time_clean.replace('.','_',)
-#         outfile = '../01_Data/01_Source/' + date_time_clean + '.json'
-
-#         # Save the data to a JSON file
-#         save_to_json(data_list, outfile)
-        
-#         # Wait for 20 seconds before making the next request
-#         time.sleep(20)
-#         current_loop += 1
 
 if __name__ == "__main__":
     main()
