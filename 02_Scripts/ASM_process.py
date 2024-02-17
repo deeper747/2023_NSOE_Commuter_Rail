3eef350a68ae1cf01a868739b75695517681e50f
deeper <60970217+deeper747@users.noreply.github.com>
2024-02-17 10:39:06 -0500

    asm process update

--- 02_Scripts/ASM_process.py
+++ 02_Scripts/ASM_process.py
@@ -1,46 +1,94 @@
 '''
-Contributors: Jia-Shen Tsai,  Wendy Wen, Zhengqi Jiao, Miaojun Pang, Alexander Yoshizumi
+Contributors: Jia-Shen Tsai, Wendy Wen, Zhengqi Jiao, Miaojun Pang,
+Alexander Yoshizumi
 
-Last Updated: 2024-01-18
+Last Updated: 2024-2-17
 
-Description: When run, the script explore the aggregated ASM data
+Description: When run, script process the raw json into one csv.
 '''
+
+# !pip install geopandas
+# !pip install shapely
+# !pip install contextily
 
 import pandas as pd
 import geopandas as gpd
-import matplotlib.pyplot as plt
-from shapely.geometry import LineString
-import contextily as ctx
+from shapely.geometry import Point
+import os
+import warnings
 
-# read the exported csv file
-df_realop = pd.read_csv('../01_data/02_Processed/realopNov12.csv')
-s = gpd.GeoSeries.from_wkt(df_realop.SHAPE) # assign the geometric column
-gdf_realop = gpd.GeoDataFrame(
-    data = df_realop,
-    geometry = s,
-    crs = 4326
+# Use the warnings context manager to capture and suppress warnings
+with warnings.catch_warnings():
+    warnings.filterwarnings("ignore")  # Ignore all warnings
+    # Define the folder path where JSON files are located
+    json_folder = '//ns-gis.win.duke.edu/StudentData/2023-2024/ct325/2023_NSOE_Commuter_Rail/01_Data/01_Source/ASM'
+
+    # Define the function to extract the lng, la, and speed only from valid dictionaries
+    def extract_coordinates(location):
+        if isinstance(location, dict):
+            return location.get('latitude'), location.get('longitude'), location.get('speed')
+        else:
+            return None, None, None
+
+    # Initialize empty lists to store the combined data
+    origin_dates = []
+    names = []
+    numbers = []
+    last_updated = []
+    longitudes = []
+    latitudes = []
+    speeds = []
+
+    # Read and process each JSON file in the folder
+    for filename in os.listdir(json_folder):
+        if filename.endswith('.json'):
+            json_filepath = os.path.join(json_folder, filename)
+
+            # Read JSON file into a df
+            df = pd.read_json(json_filepath)
+
+            # Filter routes that pass nc and make lng, lat, speed into a list of lists
+            NC_data = df.query('name in ("Silver Meteor","Carolinian","Piedmont","Palmetto","Crescent")')
+            NC_data['location'] = NC_data['location'].apply(extract_coordinates)
+
+            # Extract and store origin_date, name, and number
+            origin_dates.extend(NC_data['origin_date'].tolist())
+            names.extend(NC_data['name'].tolist())
+            numbers.extend(NC_data['number'].tolist())
+            last_updated.extend(NC_data['last_updated'].tolist())
+
+            # Extract and store lng, lat, and speed
+            latitudes.extend([location[0] for location in NC_data['location']])
+            longitudes.extend([location[1] for location in NC_data['location']])
+            speeds.extend([location[2] for location in NC_data['location']])
+
+# Create a DataFrame with columns of origin_date, name, number, longitude, latitude, speed
+df_realop = pd.DataFrame({
+    'origin_date': origin_dates,
+    'last_updated': last_updated,
+    'name': names,
+    'number': numbers,
+    'longitude': longitudes,
+    'latitude': latitudes,
+    'speed': speeds
+})
+
+df_realop = df_realop.drop_duplicates(subset=['name', 'latitude', 'longitude'])
+
+# Transform pd to gpd
+# s = [Point(xy) for xy in zip(NC_data.longitude, NC_data.latitude)]
+s = [Point(xy) for xy in zip(df_realop.longitude, df_realop.latitude)]
+gdf_realop = gpd.GeoDataFrame(data = df_realop, geometry= s, crs=4326)
+
+# Clip the GeoDataFrame to the county boundary
+gdf_county = gpd.read_file('z://2023_NSOE_Commuter_Rail/01_Data/01_Source/North_Carolina_State_and_County_Boundary_Polygons.zip')
+gdf_county = gdf_county.to_crs(4326)
+gdf_real = gpd.clip(gdf_realop, gdf_county)
+
+# gdf_real.head()
+
+# Export the geodataframe to a CSV file
+pd.DataFrame(gdf_real).to_csv(
+    '../01_Data/02_Processed/realop_total.csv',
+    index=False
 )
-gdf_realop = gdf_realop.drop(columns=['SHAPE'])  # remove duplicate gemoetric column
-gdf_realop = gdf_realop.drop_duplicates(subset=['name', 'latitude', 'longitude']) # drop duplicate observations
-
-# plot the distribution
-gdf_realop['speed'].hist(bins=40);
-
-#Create a bar plot of the total # of observations for each route
-gdf_line = gdf_realop.groupby('name')
-gdf_line['speed'].hist(bins=40);
-
-
-# plot them with a base map
-realop_plot = gdf_realop.to_crs(3857).plot(column='speed', legend=True)
-ctx.add_basemap(realop_plot,
-               source=ctx.providers.CartoDB.Voyager)
-
-# Look for potential stops at which the speed is 0
-potential_stop = gdf_realop.query('speed < 1')
-potential_stop.shape
-
-#Plot the stops with a base map
-potentialStop_plot = potential_stop.to_crs(3857).plot(column='name', legend=True)
-ctx.add_basemap(potentialStop_plot,
-               source=ctx.providers.CartoDB.Voyager)
