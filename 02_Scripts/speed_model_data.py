244e5d3e686669acd2f920794cd86e598cc71269
deeper <60970217+deeper747@users.noreply.github.com>
2024-03-13 21:40:25 -0400

    Adding march speed realop and the ML v3

--- 02_Scripts/speed_model_data.py
+++ 02_Scripts/speed_model_data.py
@@ -383,6 +383,8 @@ gdf_westbound['Distance_to_Key_Nodes_PRE'] = gdf_westbound['Node_ID'].map(gdf_mi
 
 gdf_milepost.shape
 gdf_westbound.shape
+
+
 # Snapping observations
 gdf_speed_east = gpd.read_file('../01_data/02_Processed/GIS/speed_east.shp', engine="pyogrio")
 gdf_speed_west = gpd.read_file('../01_data/02_Processed/GIS/speed_west.shp', engine="pyogrio")
@@ -401,12 +403,14 @@ east_speed_outlier_plot = gdf_speed_east[0:35].to_crs(3857).plot(column='speed',
 ctx.add_basemap(east_speed_outlier_plot,
                source=ctx.providers.CartoDB.Voyager)
 
-
 gdf_speed_east = gdf_speed_east.to_crs(gdf_milepost.crs)
 gdf_speed_west = gdf_speed_west.to_crs(gdf_westbound.crs)
 
 model_east = ckdnearest(gdf_speed_east, gdf_milepost)
 model_west = ckdnearest(gdf_speed_west, gdf_westbound)
+
+model_east = model_east.rename(columns={'speed': 'Speed_mph'})
+model_west = model_west.rename(columns={'speed': 'Speed_mph'})
 
 # The points beyond the Greensboro station will give mixed signal to the model because the H-Line is not long enoguh for distinguishing the points
 # so I am removing points beyond the station
@@ -419,18 +423,22 @@ model_east_rmol.shape
 outlier_mask = (model_west['Node_ID']>=32)
 model_west_rmol = model_west[outlier_mask]
 
-model_east_rmol = model_east_rmol.rename(columns={'speed': 'Speed_mph'})
-model_west_rmol = model_west_rmol.rename(columns={'speed': 'Speed_mph'})
-
 # Export the east-bound dataset
 ## reordering the columns
 
 model_clms = ['Speed_mph', 'Elevation_Smoothen_ft', 'Elev_Delta_CUR_ft','Elev_Delta_FOL_500ft_ft','Elev_Delta_FOL_500_1kft_ft', 'Elev_Delta_FOL_1k_1k5ft_ft', 'Elev_Delta_FOL_1k5_2kft_ft', 'Elev_Delta_FOL_2k_2k5ft_ft', 'Elev_Delta_PRE_500ft_ft', 'Elev_Delta_PRE_500_1kft_ft', 'Elev_Delta_PRE_1k_1k5ft_ft', 'Elev_Delta_PRE_1k5_2kft_ft', 'Elev_Delta_PRE_2k_2k5ft_ft', 'Curve_CUR_degree','Curve_Max_FOL_500ft_degree','Curve_Max_FOL_500_1kft_degree','Curve_Max_FOL_1k_1k5ft_degree','Curve_Max_FOL_1k5_2kft_degree','Curve_Max_FOL_2k_2k5ft_degree','Curve_Max_PRE_500ft_degree','Curve_Max_PRE_500_1kft_degree','Curve_Max_PRE_1k_1k5ft_degree','Curve_Max_PRE_1k5_2kft_degree','Curve_Max_PRE_2k_2k5ft_degree','Elev_Delta_FOL_500ft_ft','Curve_Max_FOL_500_1kft_degree','Curve_Max_FOL_1k_1k5ft_degree','Curve_Max_FOL_1k5_2kft_degree','Curve_Max_FOL_2k_2k5ft_degree','Curve_Max_PRE_500ft_degree','Curve_Max_PRE_500_1kft_degree','Curve_Max_PRE_1k_1k5ft_degree','Curve_Max_PRE_1k5_2kft_degree','Curve_Max_PRE_2k_2k5ft_degree','Distance_to_Key_Nodes_FOL', 'Distance_to_Key_Nodes_PRE', 'Station','Intersection', 'Switch', 'geometry','x_coordinate', 'y_coordinate', 'Station_if', 'Intersection_if', 'Switch_if', 'Intersection_dist', 'Station_dist','Node_ID','Cumulative_Length_ft','Distance_to_FOL_Intersection','Distance_to_FOL_Station','Distance_to_FOL_Switch','Distance_to_PRE_Intersection','Distance_to_PRE_Station','Distance_to_PRE_Switch']
 r_clms = ['Speed_mph','Curve_CUR_degree','Curve_Max_FOL_500ft_degree','Curve_Max_FOL_500_1kft_degree','Curve_Max_FOL_1k_1k5ft_degree','Curve_Max_FOL_1k5_2kft_degree','Curve_Max_FOL_2k_2k5ft_degree','Curve_Max_PRE_500ft_degree','Curve_Max_PRE_500_1kft_degree','Curve_Max_PRE_1k_1k5ft_degree','Curve_Max_PRE_1k5_2kft_degree','Curve_Max_PRE_2k_2k5ft_degree','Elev_Delta_CUR_ft' ,'Elev_Delta_FOL_500ft_ft','Elev_Delta_FOL_500_1kft_ft','Elev_Delta_FOL_1k_1k5ft_ft','Elev_Delta_FOL_1k5_2kft_ft','Elev_Delta_FOL_2k_2k5ft_ft','Elev_Delta_PRE_500ft_ft','Elev_Delta_PRE_500_1kft_ft','Elev_Delta_PRE_1k_1k5ft_ft','Elev_Delta_PRE_1k5_2kft_ft','Elev_Delta_PRE_2k_2k5ft_ft','Distance_to_FOL_Intersection','Distance_to_FOL_Station','Distance_to_FOL_Switch','Distance_to_PRE_Intersection','Distance_to_PRE_Station','Distance_to_PRE_Switch',]
 
-
 model_east_spreadsheet = model_east_rmol[model_clms]
 model_west_spreadsheet = model_west_rmol[model_clms]
+
+model_east_r = model_east_rmol[r_clms]
+model_west_r = model_west_rmol[r_clms]
+model_r = pd.concat([model_east_r,model_west_r])
+
+## if not removing outliers
+model_east_spreadsheet = model_east[model_clms]
+model_west_spreadsheet = model_west[model_clms]
 
 model_east_r = model_east_rmol[r_clms]
 model_west_r = model_west_rmol[r_clms]
